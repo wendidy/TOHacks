71c3f75434ebf97f908071608fda16bd0aa0b7c1
Eddie Pi <eddiepei2360@gmail.com>
2020-05-02 21:36:41 -0400

    Add files via upload

--- Main/static/game.js
+++ Main/static/game.js
@@ -92,12 +92,13 @@ var context = canvas.getContext('2d');
 socket.on('state', function(players) {
   context.clearRect(0, 0, 800, 600);
   context.fillStyle = 'red';
+        
   for (var id in players) {
     var player = players[id];
     context.beginPath();
     context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
     context.fill();
-    context.strokeText(player.text, player.x, player.y);
+    context.strokeText(player.text, player.x - 15, player.y - 20);
       if(seconds > 5) {
         socket.emit('msg', "");
       }
